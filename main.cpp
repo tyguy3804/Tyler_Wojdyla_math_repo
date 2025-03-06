d7a1c41fde310219230214fb07ec05bba4396d79
tyguy3804 <twojdyla@carthage.edu>
2025-03-06 13:27:14 -0600

    altered square() into power() which sets a base number to the power of an exponent number

--- main.cpp
+++ main.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
 using namespace std;
 
-void square(int num) {
-    int val = num * num;
+void power(int base, int exponent) {
+    //2^3 is 2 * 2 * 2
+    //2^5 is 2 * 2 * 2 * 2 * 2
+    int val = base;
 
-    cout << num << " squared is: " << val << endl;
+    for(int i = 0; i < exponent - 1; i++)
+    {
+        val = val * base;
+    }
+
+    cout << base << " to the power of " << exponent << " is: " << val << endl;
 }
 
 
 
 int main() {
     cout << "Github Test" << endl;
+    power(2, 3);
+    power(4, 2);
     return 0;
 };
