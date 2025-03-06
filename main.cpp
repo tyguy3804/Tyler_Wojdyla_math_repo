e9ecd600bea8f519f621b13b13bc9c52de444142
tyguy3804 <twojdyla@carthage.edu>
2025-03-06 13:22:14 -0600

    Created a square() function that prints the value of num squared

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 
+void square(int num) {
+    int val = num * num;
+
+    cout << num << " squared is: " << val << endl;
+}
+
+
+
 int main() {
     cout << "Github Test" << endl;
     return 0;
